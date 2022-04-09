db9a82a30a871d3bff16fbe59c738fac2c26c7b3
longok <huynhphuoclong2@gmail.com>
2022-04-09 11:20:25 +0700

    delete user with admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update]
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     before_action :correct_user, only: [:edit, :update]
 
     def index
@@ -43,9 +43,13 @@ class UsersController < ApplicationController
 
 
     def destroy
-        @user = User.find_by id: params[:id]
-        @user.destroy
-        redirect_to root_path
+        @user = User.find_by(id: params[:id])
+        if @user&.destroy
+            flash[:success] = "User deleted"
+        else
+            flash[:danger] = "Delete fail!"
+        end
+        redirect_to users_url
     end
 
     # Before filters
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 User.create!(name: "HP Long",
-    email:"longadmin@gmail.com",
+    email:"admin@gmail.com",
     password: "123123",
-    password_confirmation: "123123")
+    password_confirmation: "123123",
+    admin: true)
 
-99.times do |n|
+50.times do |n|
     name = Faker::Name.name 
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
