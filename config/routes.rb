babd607e77111857d992182c4412b44b3b09a8cd
longok <huynhphuoclong2@gmail.com>
2022-04-13 07:36:44 +0700

    reset-password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   get 'static_pages/home'
   get 'static_pages/help'
 
@@ -15,5 +16,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
   root 'static_pages#home'
 end
