143242f4f158325f3184d4001feb9d3a8e05a4e1
vindt <thanhvinh0195.cntt@gmail.com>
2022-05-29 20:48:11 +0700

    :bug: wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception 
+    protect_from_forgery with: :exception
     before_action :current_cart
     before_action :current_cart_noti
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,28 +4,15 @@ class Cart < ApplicationRecord
     has_many :products, through: :bills
 
     def total_cart_item
-        sum = 0
-        self.cart_items.each do |cart_item|
-          sum += cart_item.total_pirce
-        end
-        return sum
+      cart_items.pluck(:total_pirce).sum
     end
 
     def total_quantity
-        quantity = 0
-        self.cart_items.each do |cart_item|
-          quantity += cart_item.quantity
-        end
-        return quantity
+      cart_items.pluck(:quantity).sum
     end
 
     def add_product(product)
-      current_item = cart_items.find_by product_id: (product)
-      if current_item 
-          current_item.quantity += 1
-      else
-          current_item = cart_items.build(:product_id => product.id)
-      end
-      current_item
+      current_item = cart_items.find_or_initialize_by(product_id: product)
+      current_item.quantity += 1
     end
 end
